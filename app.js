124e2e04cb04dc80b1ac2f78546fca5505488f0c
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2024-05-12 09:58:29 +0400

    Add files

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * @author Ashvin Motye
+ * @date 12 May 2024
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+});
+
+// init
+const init = () => {
+    console.log('init');
+}
+// END init
